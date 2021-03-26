281f8c243ade88d0dca19f2084c7d5e05390c53e
ChainDot <crypto.dot@protonmail.com>
2021-03-26 19:46:26 +0100

    update

--- wc.js
+++ wc.js
@@ -1,92 +1,67 @@
 const fs = require('fs')
-// exit cases
+//command line lenght exit case
 if(process.argv.length < 3 || process.argv.length > 4){
   console.log('usage: node wc.js (optional[-l,-w,-c]) file.txt')
   process.exit(1)
-} 
-
-// all
+}
+// show all 
 if(process.argv.length === 3){
 // check if the file exist
-const fileExist = fs.existsSync(process.argv[2])
+let fileExist = fs.existsSync(process.argv[2])
 if(!fileExist){
   console.log(`error: ${process.argv[2]} does not exist`)
   process.exit(1)
 } 
 // check if it's a file
-const isFile = fs.statSync(process.argv[2]) 
+let isFile = fs.statSync(process.argv[2]) 
 if(!isFile.isFile()){
   console.log(`error: ${process.argv[2]} is not a file`)
   process.exit(1)
 } 
-
-let read = fs.readFileSync(process.argv[2], 'utf-8');
-// number of line
+let read = fs.readFileSync(process.argv[2], 'utf-8')
+  // number of line
 let line = read.split('\n').length
-//number of word
+  //number of word
 let word = ((read.replaceAll('\n',' ')).split(' ')).length
-// number of letter
+  // number of letter
 let character = (read.split('')).length
+
 console.log(` ${line}  ${word}  ${character} ${process.argv[2]}`)
-}
-
-// line
-if(process.argv.length === 4 && process.argv[2] === '-l'){
-// check if the file exist
-const fileExist = fs.existsSync(process.argv[3])
-if(!fileExist){
+  
+  // show individually
+} else if(process.argv.length === 4){
+  
+  // check if the file exist
+  let fileExist = fs.existsSync(process.argv[3])
+  if(!fileExist){
   console.log(`error: ${process.argv[3]} does not exist`)
   process.exit(1)
-} 
-// check if it's a file
-const isFile = fs.statSync(process.argv[3]) 
-if(!isFile.isFile()){
+  } 
+  // check if it's a file
+  const isFile = fs.statSync(process.argv[3]) 
+  if(!isFile.isFile()){
   console.log(`error: ${process.argv[3]} is not a file`)
   process.exit(1)
-} 
-
+  } 
+   // read file 
   let read = fs.readFileSync(process.argv[3], 'utf-8')
-  let line = read.split('\n').length
-  console.log(` ${line} ${process.argv[3]}`)
-  process.exit(1)
-} 
-
-// word
-if (process.argv.length === 4 && process.argv[2] === '-w'){
- // check if the file exist 
-  const fileExist = fs.existsSync(process.argv[3])
-if(!fileExist){
-  console.log(`error: ${process.argv[3]} does not exist`)
-  process.exit(1)
+  // parameter line -l
+  if(process.argv[2] === '-l'){
+  // number of line
+    let line = read.split('\n').length
+    console.log(` ${line} ${process.argv[3]}`)
+  } else if ( process.argv[2] === '-w'){
+  // number of words
+    let word = ((read.replaceAll('\n',' ')).split(' ')).length
+    console.log(` ${word} ${process.argv[3]}`)
+  } else if ( process.argv[2] === '-c'){
+  // number of characters
+    let character = (read.split('')).length
+    console.log(` ${character} ${process.argv[3]}`)
+  } else {
+  // check exit case optional parameters
+    console.log('usage: node wc.js (optional[-l,-w,-c]) file.txt')
+    process.exit(1)
+  }
 }
-// check if it's a file
-const isFile = fs.statSync(process.argv[3]) 
-if(!isFile.isFile()){
-  console.log(`error: ${process.argv[3]} is not a file`)
-  process.exit(1)
-} 
 
-  let read = fs.readFileSync(process.argv[3], 'utf-8')
-  let word = ((read.replaceAll('\n',' ')).split(' ')).length
-  console.log(` ${word} ${process.argv[3]}`)
-} 
-
-//character
-if (process.argv.length === 4 && process.argv[2] === '-c'){
-  // check if the file exist 
-const fileExist = fs.existsSync(process.argv[3])
-if(!fileExist){
-  console.log(`error: ${process.argv[3]} does not exist`)
-  process.exit(1)
-} 
-// check if it's a file
-const isFile = fs.statSync(process.argv[3]) 
-if(!isFile.isFile()){
-  console.log(`error: ${process.argv[3]} is not a file`)
-  process.exit(1)
-} 
-
-  let read = fs.readFileSync(process.argv[3], 'utf-8')
-  let character = (read.split('')).length
-  console.log(` ${character} ${process.argv[3]}`)
-}
